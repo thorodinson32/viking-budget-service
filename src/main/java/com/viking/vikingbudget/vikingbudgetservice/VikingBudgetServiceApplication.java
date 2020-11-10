320470e6a3049d1bfdf53e676b1f3324ade47c4f
Jacob Morgan <jacob.morgan@autozone.com>
2020-11-10 13:44:36 -0600

    Initial commit

---
+++ src/main/java/com/viking/vikingbudget/vikingbudgetservice/VikingBudgetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.viking.vikingbudget.vikingbudgetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikingBudgetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VikingBudgetServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/viking/vikingbudget/vikingbudgetservice/VikingBudgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viking.vikingbudget.vikingbudgetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikingBudgetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
